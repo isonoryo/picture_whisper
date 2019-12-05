130675b5090b50858452488b4f3f8b45bc2a288e
isonoryo <ljiye2422@gmail.com>
2019-12-05 14:42:18 +0900

    写真投稿機能実装とuserとfeedのアソシエーション実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :feeds do
+    collection do
+      post :confirm
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_024221) do
+ActiveRecord::Schema.define(version: 2019_12_05_021736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_12_04_024221) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "feeds", "users"
 end
